e67af74d0eb347bcf4f823bdf738e0f805001618
Albert Mikaelyan <albert@websplanet.com>
2014-11-24 16:38:10 +0200

    test bootstrap 1

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
 <head>
 <meta charset="UTF-8">
 <title></title>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 <link rel="stylesheet" href="css/index.css">
 </head>
 
@@ -64,6 +74,10 @@ div class="site-wrapper">
       </div>
     </div>
 </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
