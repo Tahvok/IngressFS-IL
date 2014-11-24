e02f7ffd06caf520d2463349b201cdd1e85efb07
Albert Mikaelyan <albert@websplanet.com>
2014-11-24 17:22:46 +0200

    test grid 1

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title></title>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
 
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -12,73 +12,73 @@
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+      <link rel="stylesheet" href="css/index.css">
+  </head>
 
-<link rel="stylesheet" href="css/index.css">
-</head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Cover</h3>
 
-<body>
-<div class="site-wrapper">
-  <div class="site-wrapper-inner">
-    <div class="cover-container">
-      <div class="masthead clearfix">
-        <div class="inner">
-          <h3 class="masthead-brand">Cover</h3>
+              <ul class="nav masthead-nav">
+                <li class="active">
+                  <a href="http://bootsnipp.com/iframe/g6GWQ" target=
+                  "_blank">View full screen</a>
+                </li>
 
-          <ul class="nav masthead-nav">
-            <li class="active">
-              <a href="http://bootsnipp.com/iframe/g6GWQ" target=
-              "_blank">View full screen</a>
-            </li>
+                <li>
+                  <a href="#">Features</a>
+                </li>
 
-            <li>
-              <a href="#">Features</a>
-            </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+          <div class="inner cover">
+            <h1 class="cover-heading">Full screen background cover page.</h1>
 
-      <div class="inner cover">
-        <h1 class="cover-heading">Full screen background cover page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and
+            beautiful home pages. Download, edit the text, and add your own
+            fullscreen background color and photo to make it your own.</p>
 
-        <p class="lead">Cover is a one-page template for building simple and
-        beautiful home pages. Download, edit the text, and add your own
-        fullscreen background color and photo to make it your own.</p>
+            <p class="lead"><a class="btn btn-lg btn-info" href="#">Learn
+              more</a></p>
+          </div>
 
-        <p class="lead"><a class="btn btn-lg btn-info" href="#">Learn
-        more</a></p>
-      </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <!-- Validation -->
 
-      <div class="mastfoot">
-        <div class="inner">
-          <!-- Validation -->
+              <p><a href=
+              "http://validator.w3.org/check?uri=http%3A%2F%2Fbootsnipp.com%2Fiframe%2Fg6GWQ"
+                 target="_blank"><small>HTML</small><sup>5</sup></a></p>
 
-          <p><a href=
-          "http://validator.w3.org/check?uri=http%3A%2F%2Fbootsnipp.com%2Fiframe%2Fg6GWQ"
-          target="_blank"><small>HTML</small><sup>5</sup></a></p>
-          
-          
-          <p><a href=
-          "https://github.com/twbs/bootlint"
-          target="_blank"><small>Checked with Bootlint</small></a></p>          
-      
-          
 
-          <p>© 2014 Your Name ~ <a href=
-          "http://getbootstrap.com/">Bootstrap</a></p>
+              <p><a href=
+              "https://github.com/twbs/bootlint"
+                 target="_blank"><small>Checked with Bootlint</small></a></p>          
+
+
+
+              <p>© 2014 Your Name ~ <a href=
+              "http://getbootstrap.com/">Bootstrap</a></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-</div>
-</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
-</body>
+  </body>
 
 </html>
