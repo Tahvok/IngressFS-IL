22a502a4e498fbbad4ddb6ae058fd10e054e622e
Albert Mikaelyan <albert@websplanet.com>
2014-11-24 18:51:17 +0200

    text modified

--- index.php
+++ index.php
@@ -18,63 +18,34 @@
   </head>
 
   <body>
-    <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-          <div class="masthead clearfix">
-            <div class="inner">
-              <h3 class="masthead-brand">Cover</h3>
-
-              <ul class="nav masthead-nav">
-                <li class="active">
-                  <a href="http://bootsnipp.com/iframe/g6GWQ" target=
-                  "_blank">View full screen</a>
-                </li>
-
-                <li>
-                  <a href="#">Features</a>
-                </li>
-
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="inner cover">
-            <h1 class="cover-heading">Full screen background cover page.</h1>
-
-            <p class="lead">Cover is a one-page template for building simple and
-            beautiful home pages. Download, edit the text, and add your own
-            fullscreen background color and photo to make it your own.</p>
-
-            <p class="lead"><a class="btn btn-lg btn-info" href="#">Learn
-              more</a></p>
-          </div>
-
-          <div class="mastfoot">
-            <div class="inner">
-              <!-- Validation -->
-
-              <p><a href=
-              "http://validator.w3.org/check?uri=http%3A%2F%2Fbootsnipp.com%2Fiframe%2Fg6GWQ"
-                 target="_blank"><small>HTML</small><sup>5</sup></a></p>
-
-
-              <p><a href=
-              "https://github.com/twbs/bootlint"
-                 target="_blank"><small>Checked with Bootlint</small></a></p>          
-
-
-
-              <p>© 2014 Your Name ~ <a href=
-              "http://getbootstrap.com/">Bootstrap</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+  <p class="lead">
+    It's Time To Change The Future!
+</p>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<p class="content">
+Control your immediate surroundings, Discover interesting places
+around your neighborhood and everything in one challenging.game.
+Meet and collaborate with players from all around the globe.<br />
+Play. Lead. Influence.
+</p>
+</div>
+</div>
+<div class="row">
+  <div class="col-md-12">asdasd</div>
+</div>
+<div class="row">
+  <div class="col-md-4">asdasd</div>
+  <div class="col-md-4">asdasd</div>
+  <div class="col-md-4">asdasd</div>
+</div>
+</div>
+  </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
