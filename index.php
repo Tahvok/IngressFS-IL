1cfd5544ec7ceeb783ec9ff202983845b8f6d868
Albert Mikaelyan <albert@websplanet.com>
2014-11-25 10:52:28 +0200

    Lionel Fix

--- index.php
+++ index.php
@@ -29,30 +29,25 @@
       <div class="row">
         <div class="col-md-12">
           <p class="content">
-          Control your immediate surroundings, Discover interesting<br/>
-          places around your neighborhood and everything in one<br/>
-          challenging game. Meet and collaborate with players from<br/>
-          all around the globe.<br/>
+          Control your immediate surroundings, Discover interesting places around your neighborhood and everything in one challenging game. Meet and collaborate with players from all around the globe.<br/>
           Play. Lead. Influence.
           </p>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-6">
-          <img src="images/FS_fs.png"></img>
-        </div>
-        <div class="col-md-6">
-          <div class="relate">
-            <p class="israel">
-            Israel
-            </p>
-          </div>
+        <div class="col-md-12 clearfix">
+			<div class="center clearfix">
+				<img src="images/FS_fs.png" class="pull-left">
+				<p class="israel pull-left">
+					Israel
+				</p>
+			</div>
         </div>
       </div>
-      <div class="row">
+      <div class="row bottom-row">
         <div class="col-md-4">asdasd</div>
-        <div class="col-md-4">asdasd</div>
-        <div class="col-md-4">asdasd</div>
+        <div class="col-md-4"><div class="text-center">asdasd</div></div>
+        <div class="col-md-4"><div class="pull-right">asdasd</div></div>
       </div>
     </div>
     </div>
