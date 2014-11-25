1f3bcf4b7304b4a5c877d1bdd876f8c871a7ca59
Albert Mikaelyan <albert@websplanet.com>
2014-11-25 16:09:19 +0200

    RC changes

--- index.php
+++ index.php
@@ -1,60 +1,69 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title></title>
+        <head>
+                <meta charset="UTF-8">
+                <title></title>
 
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+                <!-- Bootstrap -->
+                <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-      <link rel="stylesheet" href="css/index.css">
-  </head>
+                <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                <!--[if lt IE 9]>
+                        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+                        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+                        <![endif]-->
+                        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+                        <link rel="stylesheet" href="css/index.css">
+        </head>
 
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <p class="lead">
-          It's Time To Change The Future!
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <p class="content">
-          Control your immediate surroundings, Discover interesting places around your neighborhood and everything in one challenging game. Meet and collaborate with players from all around the globe.<br/>
-          Play. Lead. Influence.
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 clearfix">
-			<div class="center clearfix">
-				<img src="images/FS_fs.png" class="pull-left">
-				<p class="israel pull-left">
-					Israel
-				</p>
-			</div>
-        </div>
-      </div>
-      <div class="row bottom-row">
-        <div class="col-md-4">asdasd</div>
-        <div class="col-md-4"><div class="text-center">asdasd</div></div>
-        <div class="col-md-4"><div class="pull-right">asdasd</div></div>
-      </div>
-    </div>
-    </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-  </body>
+        <body>
+                <div class="container-fluid">
+                        <div class="row">
+                                <div class="col-md-12">
+                                        <p class="lead">
+                                        It's Time To Change The Future!
+                                        </p>
+                                </div>
+                        </div>
+                        <div class="row">
+                                <div class="col-md-12">
+                                        <p class="content">
+                                        Control your immediate surroundings, Discover interesting places around your neighborhood and everything in one challenging game. Meet and collaborate with players from all around the globe.<br/>
+                                        Play. Lead. Influence.
+                                        </p>
+                                </div>
+                        </div>
+                        <div class="row">
+                                <div class="col-md-12 clearfix">
+                                        <div class="center clearfix">
+                                                <img src="images/FS_clear3.png" class="pull-left">
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="row">
+                                <div class="col-md-12">
+                                        <p class="time">6 | DEC | 2014</p>
+                                </div>
+                        </div>
+                        <div class="row buttons">
+                                <div class="col-md-6">
+                                        <a href="aaa" style="margin-top:70px;"><img src="images/FS_playstore.png" /></a>
+                                        <a href="aaa"><img src="images/FS_appstore.png" /></a>
+                                </div>
+                                <div class="col-md-6"><div class="pull-right"></div></div>
+                        </div>
+                        <div class="row">
+                                <div class="col-md-12">
+                                        <p class="conclusion">The World Around You Is Not What It Seems...</p>
+                                </div>
+                        </div>
+                </div>
+                </div>
+                <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                <!-- Include all compiled plugins (below), or include individual files as needed -->
+                <script src="bootstrap/js/bootstrap.min.js"></script>
+        </body>
 
 </html>
